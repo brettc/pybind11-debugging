b60f33713e6cb685e468654f923f17832723ebe1
Brett Calcott <brett.calcott@gmail.com>
2022-06-17 15:25:59 +1200

    feat: add cats/dogs failing example

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,102 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <memory>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
+int add(int i, int j) { return i + j; }
+
+class Animal {
+   public:
+    virtual ~Animal() {}
+    virtual std::string go(int n_times) = 0;
+};
+using Animal_ptr = std::shared_ptr<Animal>;
+using Animal_vec = std::vector<Animal_ptr>;
+
+std::string call_go(Animal *animal) { return animal->go(3); }
+
+class Dog : public Animal {
+   public:
+    std::string go(int n_times) override {
+        std::string result;
+        for (int i = 0; i < n_times; ++i) result += "woof! ";
+        return result;
+    }
+};
+
+Animal_vec dog_list() {
+    Animal_vec dogs;
+    dogs.push_back(std::make_shared<Dog>());
+    dogs.push_back(std::make_shared<Dog>());
+    dogs.push_back(std::make_shared<Dog>());
+    return dogs;
 }
+
+using string_vec = std::vector<std::string>;
+
+string_vec call_go_list(Animal_vec const &animals) {
+    string_vec calls;
+
+    for (const auto &p : animals) {
+        calls.push_back(p->go(1));
+    }
+
+    return calls;
+}
+
+class PyAnimal : public Animal {
+   public:
+    /* Inherit the constructors */
+    using Animal::Animal;
+
+    /* Trampoline (need one for each virtual function) */
+    std::string go(int n_times) override {
+        PYBIND11_OVERRIDE_PURE(
+            std::string, /* Return type */
+            Animal,      /* Parent class */
+            go,          /* Name of function in C++ (must match Python name) */
+            n_times      /* Argument(s) */
+        );
+    }
+};
+
+class Pound {
+   public:
+    Animal_vec animals_;
+    explicit Pound(Animal_vec animals) : animals_(std::move(animals)) {}
+    string_vec go() { return call_go_list(animals_); }
+};
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
     m.def("add", &add);
     m.def("subtract", [](int i, int j) { return i - j; });
+    py::class_<Animal, PyAnimal, std::shared_ptr<Animal>>(m, "Animal")
+        .def(py::init<>())
+        .def("go", &Animal::go)
+        //
+        ;
+
+    py::class_<Dog, Animal, std::shared_ptr<Dog>>(m, "Dog")
+        //
+        .def(py::init<>())
+        //
+        ;
+
+    py::class_<Pound>(m, "Pound")
+        //
+        .def(py::init<Animal_vec>())
+        .def("go", &Pound::go)
+        //
+        ;
+
+    m.def("call_go", &call_go);
+    m.def("dog_list", &dog_list);
+    m.def("call_go_list", &call_go_list);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -11,3 +11,59 @@ def test_add():
 
 def test_sub():
     assert m.subtract(1, 2) == -1
+
+
+class Cat(m.Animal):
+    def go(self, n_times):
+        return "meow! " * n_times
+
+
+def test_dog():
+    c = Cat()
+    assert m.call_go(c) == u'meow! meow! meow! '
+
+
+def test_dog_list():
+    class Cat(m.Animal):
+        def go(self, n_times):
+            return "meow! " * n_times
+
+    dl = m.dog_list()
+    print(m.call_go_list(dl))
+
+    dl.append(Cat())
+    print(m.call_go_list(dl))
+
+
+def test_cat_list():
+    cats = [Cat() for _ in range(3)]
+    print(m.call_go_list(cats))
+
+
+def test_pound():
+    # This works, but is flawed!
+    cats = [Cat() for _ in range(3)]
+    p = m.Pound(cats)
+    print(p.go())
+
+
+def test_pound_with_del_cats():
+    cats = [Cat() for _ in range(3)]
+    p = m.Pound(cats)
+
+    # This appears to kill off *all* the Cats. Just leaving the PyAnimal class
+    # which can no longer locate the python objects.
+    del cats
+
+    # Virtual Call!
+    print(p.go())
+
+
+def test_pound_with_del_dogs():
+    dogs = [m.Dog() for _ in range(3)]
+    p = m.Pound(dogs)
+
+    del dogs
+
+    # This works okay.
+    print(p.go())
